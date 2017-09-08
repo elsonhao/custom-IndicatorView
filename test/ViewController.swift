f13c8bd9dcccceffd96fe3e173ec76c8158e8931
T814 <t814@T814de-MacBook-Pro.local>
2017-09-08 10:55:35 +0800

    自訂轉圈圈畫面

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -10,15 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var loadingContainer: UIView = UIView()
+    var loadingView: UIView = UIView()
+    var actInd: UIActivityIndicatorView = UIActivityIndicatorView()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        showActivityIndicatory(uiView: self.view)
+        
+    }
+    
+    func showActivityIndicatory(uiView: UIView) {
+        
+        loadingContainer.frame = uiView.frame
+        loadingContainer.center = uiView.center
+        loadingContainer.backgroundColor = UIColor.clear
+        
+        
+        
+        loadingView.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
+        loadingView.center = uiView.center
+       
+        loadingView.backgroundColor = UIColor.lightGray
+        
+        loadingView.clipsToBounds = true
+        loadingView.layer.cornerRadius = 10
+        
+        
+        actInd.frame = CGRect(x: 0, y: 0, width: 40, height: 40);
+        actInd.activityIndicatorViewStyle =
+            UIActivityIndicatorViewStyle.whiteLarge
+        actInd.center = CGPoint(x: loadingView.frame.size.width / 2, y: loadingView.frame.size.height / 2)
+        
+        loadingView.addSubview(actInd)
+        loadingContainer.addSubview(loadingView)
+        uiView.addSubview(loadingContainer)
+        actInd.startAnimating()
+    }
+  
+    func hideActivityIndicator(uiView: UIView) {
+        actInd.stopAnimating()
+        loadingContainer.removeFromSuperview()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
